8ae917fdace5264ee2ec4a66cb00f3141c4cb8db
Johnny Qu <johnnyqu4@gmail.com>
2020-05-02 23:19:22 -0400

    YEET IT'S WORKING

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 const electron = require("electron");
+const activeWin = require("active-win");
 
-const {app, ipcMain} = electron;
+const {app} = electron;
 
-app.on("ready", function(){
-
+app.on("ready", async function(){
+    console.log("reee");
+    while(true){
+        console.log(await activeWin());
+        await sleep(1000);
+    }
 });
+function sleep(ms){
+    return new Promise(function(resolve){
+        setTimeout(resolve, ms);
+    });
+}
