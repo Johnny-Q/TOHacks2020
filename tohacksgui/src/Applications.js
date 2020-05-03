2bd5ecdc13dae6f741fe5191e5695455a41295b5
smilesthesad <vzheng@gmail.com>
2020-05-03 06:22:49 -0400

    navbar code switching to vanilla js

--- tohacksgui/src/Applications.js
+++ tohacksgui/src/Applications.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import {ipcRenderer} from 'electron'
+
 
 class Applications extends React.Component{
+    constructor(props){
+        super(props);
+        this.handleOnClick = this.handleOnClick.bind(this);
+    }
+    
+    handleOnClick(){
+        console.log('handleOnClick')
+        ipcRenderer.send("yeet", "ping");
+    }
     render(){
-        return(<div><h1 class = "Header">Applications</h1></div>)
+        return(<div><button onClick = {this.handleOnClick}>send info</button>
+            <h1 class = "Header">Applications</h1></div>)
     }
 }
 export default Applications
