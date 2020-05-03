24ec8384d2c5a87339645d29e71de4cf3d4c6885
Johnny Qu <johnnyqu4@gmail.com>
2020-05-03 05:59:01 -0400

    twitter stuff working

--- apis.js
+++ apis.js
@@ -1,13 +1,49 @@
 const axios = require("axios");
 const t = require("twit");
+const fs = require("fs");
 //twitter
-var params = {
-    method: 'POST',
-    url:"https://api.twitter.com/oauth/request_token",
-    headers:{
-        "Authentication": 'OAuth oauth_nonce="yeeeeeeeeet", oauth_callback="http%3A%2F%2Flocaltest.me%3A3000", oauth_signature_method="HMAC-SHA1", oauth_consumer_key="OqEqJeafRSF11jBMStrZz", oauth_signature="Pc%2BMLdv028fxCErFyi8KXFM%2BddU%3D", oauth_version="1.0"'      
-    },
+
+function tRandomPost(t){
+    if(Math.random() > 0.5){
+        var messages = ["first person to dm me, I will buy you bubble tea for a week", "despite the constant negative press covfefe", "sometimes when I'm too lazy to make dinner, I will take my cats dry food", "I wear adult diapers"];
+        tPost(t, messages[Math.floor(Math.random()*messages.length)]);
+    }else{
+        var images = ["reeeee.png"];
+        tPostImage(t, images[Math.floor(Math.random()*images.length)]);
+    }
+}
+
+function tPost(t, data){
+    t.post('statuses/update', { status: data }, function(err, data, response) {
+        console.log(data);
+    });
+}
+function tPostImage(t, imgPath){
+    var b64content = fs.readFileSync('./pictures/' + imgPath, { encoding: 'base64' })
+ 
+    t.post('media/upload', { media_data: b64content }, function (err, data, response) {
+        console.log(data);
+        fs.writeFileSync("asdf.txt", data);
+        var mediaIdStr = data.media_id_string
+        var altText = "why was I unproductive"
+        var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+        
+        t.post('media/metadata/create', meta_params, function (err, data, response) {
+            console.log(data);
+            fs.writeFileSync("asdfasdf.txt", data);
+            if (!err) {
+                var params = { status: 'felt cute might delete', media_ids: [mediaIdStr] }
+            
+                t.post('statuses/update', params, function (err, data, response) {
+                    console.log(data)
+                })
+            }
+        })
+        })
+}
+
+module.exports = {
+    tRandomPost,
+    tPost,
+    tPostImage
 };
-(async ()=>{
-    await axios(params);
-})()
--- main.js
+++ main.js
@@ -2,33 +2,30 @@ const AWPoll = require("./polling.js")
 const electron = require("electron");
 const activeWin = require("active-win");
 const api = require("./apis.js");
+const twit = require("twit");
+const config = require("./config.json");
 
 const {app, BrowserWindow} = electron;
 
+var t = twit({
+  consumer_key: config.API_key,
+  consumer_secret: config.API_key_secret,
+  access_token: config.access_token,
+  access_token_secret: config.access_token_secret,
+});
 
+let winpoll;
 
 app.on("ready", async function(){
     var window = new BrowserWindow();
     window.loadURL("http://localhost:3000")
     console.log("reee");
-})
-
-
-
-
-const api = require("./apis.js");
-
-
-const {app} = electron;
-let winpoll;
-
-app.on("ready", async function(){
-    console.log("reee");
-    winpoll = new AWPoll.ActiveWindowPoll(activeWin, 1, 100);
-    winpoll.start();
-    await sleep(10000);
-    winpoll.print();
-    console.log(winpoll.getStats(10));
+    api.tRandomPost(t, "reeeee.png");
+    // winpoll = new AWPoll.ActiveWindowPoll(activeWin, 1, 100);
+    // winpoll.start();
+    // await sleep(10000);
+    // winpoll.print();
+    // console.log(winpoll.getStats(10));
 });
 
 function sleep(ms){
