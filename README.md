ce9ffc3b3ff1276e49f0bfca561bb9a0b1a8cd7f
Johnny Qu <johnnyqu4@gmail.com>
2020-05-02 22:12:17 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TOHacks2020
 TOHacks2020
+run npm i
+npm start to start the app
---
+++ main.js
@@ -0,0 +1,7 @@
+const electron = require("electron");
+
+const {app, ipcMain} = electron;
+
+app.on("ready", function(){
+
+});
